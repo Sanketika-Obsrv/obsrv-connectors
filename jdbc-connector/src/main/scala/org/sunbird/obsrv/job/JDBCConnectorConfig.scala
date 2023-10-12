03bed80349d3abfff0cc37fa292aa601e34dd308
shiva-rakshith <rakshiths@sanketika.in>
2023-10-12 15:20:28 +0530

    Update JDBCConnectorConfig.scala

--- jdbc-connector/src/main/scala/org/sunbird/obsrv/job/JDBCConnectorConfig.scala
+++ jdbc-connector/src/main/scala/org/sunbird/obsrv/job/JDBCConnectorConfig.scala
@@ -5,7 +5,7 @@ import com.typesafe.config.Config
 class JDBCConnectorConfig (config: Config, args: Array[String]){
 
   if (args.length == 0)
-    throw new Exception("Please provide arguments..")
+    throw new Exception("Please provide dataset id as argument..")
 
   val datasetId: String = args(0)
   val kafkaServerUrl: String = config.getString("kafka.broker-servers")
